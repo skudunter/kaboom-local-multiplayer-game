84e39509a81bab5c5e5bf335a299e95bd275876c
skudunter <danielgruttercpt@gmail.com>
2023-05-29 21:55:55 +0200

    feat added assets

--- src/levels/1.ts
+++ src/levels/1.ts
@@ -1,7 +1,7 @@
 import k, { tileWidth, tileHeight } from "../constants";
 
 function handleHit(player: any) {
-  let t = 0;
+//   let t = 0;
   player.on("hurt", () => {
     player.color = { r: 255, g: 255, b: 255 };
     console.log("hurts");
@@ -37,12 +37,12 @@ const level1 = k.scene("level1", () => {
       "=                       =",
       "=                       =",
       "=                       =",
+      "=#         c           @=",
       "=                       =",
       "=                       =",
-      "= #                    @=",
       "=                       =",
       "=                       =",
-      "=                       =",
+      "= bf                    =",
       "===================================",
     ],
     {
@@ -73,10 +73,14 @@ const level1 = k.scene("level1", () => {
           k.z(100),
           k.health(10),
         ],
-        " ": () => [k.sprite("floor1"), k.anchor("center"), k.scale(5.1)],
+        " ": () => [k.sprite("floor3"), k.anchor("center"), k.scale(5.1)],
+        "b": () => [k.sprite("floor1"), k.anchor("center"), k.scale(5.1)],
+        "f": () => [k.sprite("floor2"), k.anchor("center"), k.scale(5.1)],
+        "c": () => [k.sprite("crate1"), k.anchor("center"), k.scale(5.1),k.body({mass:10}),k.area(),k.z(100)],
       },
     }
   );
+  //walls for collision purposes
   k.add([k.rect(k.width(), tileWidth), k.pos(0, 0), k.color(0, 0, 0),k.body({isStatic:true}),k.area(),k.z(-1)]);
   k.add([k.rect(tileWidth,k.height()-tileHeight*2-20), k.pos(0, tileHeight+1), k.color(0, 0, 0),k.body({isStatic:true}),k.area(),k.z(-1)]);
   k.add([k.rect(k.width(), tileHeight), k.pos(0, k.height()-tileHeight-16), k.color(0, 0, 0),k.body({isStatic:true}),k.area(),k.z(-1)]);
@@ -84,7 +88,9 @@ const level1 = k.scene("level1", () => {
 
   const player1 = level.get("robot1")[0];
   const player2 = level.get("robot2")[0];
-
+ 
+  k.add([k.sprite('floor5'),k.pos(player1.pos.x,player1.pos.y),k.scale(5.1),k.z(-1)])
+  k.add([k.sprite('floor5'),k.pos(player2.pos.x,player2.pos.y),k.scale(5.1),k.z(-1)])
   handlePlayerIdleAnimation(player1);
   handlePlayerIdleAnimation(player2);
 
